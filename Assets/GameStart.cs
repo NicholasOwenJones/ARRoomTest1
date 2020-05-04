5b8f14a97ab6dd537405f32502797c7066e04533
Nick <NJ144@live.mdx.ac.uk>
2020-05-04 18:34:03 +0100

    centre2
    centre2

---
+++ Assets/GameStart.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameStart : MonoBehaviour
+{
+    public GameObject player;
+
+    public GameObject Character1;
+    public GameObject Character2;
+    public GameObject Character3;
+
+    public float diagonalLenth; 
+
+    // Objects that are found in scene.
+
+    public GameObject cube1;
+
+    public GameObject cube2;
+
+    public GameObject centre;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        centre = GameObject.FindGameObjectWithTag("centre");
+        cube1 = GameObject.FindGameObjectWithTag("Cube2");
+        cube2 = GameObject.FindGameObjectWithTag("Cube1");
+        diagonalLenth = Vector3.Distance(cube1.transform.position, cube2.transform.position);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
